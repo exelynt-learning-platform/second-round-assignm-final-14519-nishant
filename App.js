6e13f7294877aebf911c2501899a9625e0a8e8cc
hambirNishant <143251007+hambirNishant@users.noreply.github.com>
2026-04-01 15:11:15 +0530

    Update App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ChatBox from "./Components/ChatBox";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="title">Chatbox AI 🤖</h1>
+      <ChatBox />
+    </div>
+  );
+}
+
+export default App;
